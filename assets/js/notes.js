f6c79f8eccbf6b23b82a29044484ecf19a17e4f3
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-07 17:04:18 +0200

    started with notes section

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -1,8 +1,13 @@
-function notesFunction() {
+
+/*function notesFunction() {
     var x = document.getElementsByClassName("notes_wrapper");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
+  }*/
+
+  function getNewNote(){
+    
   }
