d2a032471207e36deafdef92303923459fadb6b6
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-09 20:02:22 +0200

    testing js code and made changes in html p to div

--- assets/js/section-links.js
+++ assets/js/section-links.js
@@ -1,29 +1,9 @@
 /* this section is for the section links */
 /* when you click on one of the links that link will be displayed in the section frame */
 
-function searchFunction() {
-    var x = document.getElementsByClassName("search_wrapper");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function notesFunction() {
-    var x = document.getElementsByClassName("notes_wrapper");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function contactFunction() {
-    var x = document.getElementsByClassName("contact_wrapper");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+$(function() {
+  $('.showSingle').click(function() {
+    $('.targetDiv').hide();
+    $('#div' + $(this).attr('target')).show();
+  });
+});
