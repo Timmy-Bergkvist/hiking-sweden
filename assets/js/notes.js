b50b4fc450660a077d447621f26f7441ace64fb7
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-11 22:34:41 +0200

    testing code

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -1,2 +1,24 @@
 
+/*
+$("#noteBody").on("keyup",function(e){
 
+    if(e.keyCode == 13 && $("#noteBody").val() !="")
+    {
+    var noteDisplay = $("<div class='noteDisplay'></div>").text($("#noteBody").val());
+    var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
+        var p = $(this).parent();
+        p.fadeOut(function(){
+            p.remove();
+        });
+    });
+
+    task.append(del);
+    $("#read_form").append(noteDisplay);
+    //clear the input
+    $("#noteBody").val("");
+    }
+});*/
+
+$("#btnSave").click(function(){
+    ("#noteBody").keyup();
+});
