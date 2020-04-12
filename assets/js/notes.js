1d1d210aa4c54ef29623c91b3d0504033125b05c
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-12 17:40:34 +0200

    trying to fix notes.js file and made changes in css

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -18,7 +18,63 @@ $("#noteBody").on("keyup",function(e){
     $("#noteBody").val("");
     }
 });*/
-
+/*
 $("#btnSave").click(function(){
-    ("#noteBody").keyup();
+    
 });
+*/
+
+const note_button = document.querySelector('.note_button');
+let noteBody = document.querySelector('.noteBody');
+const read_form = document.querySelector('.read_form');
+
+class note_display{
+
+    constructor(itemName){
+        //create the item div
+        this.createDiv(itemName);
+    }
+    createDiv(itemName){
+        let noteBody = document.createElement('noteBody');
+        noteBody.value = itemName;
+        noteBody.disabled = true;
+        noteBody.classList.add('item_input');
+        noteBody.type = "text";
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('read_form');
+
+        let removeBtn = document.createElement('button');
+        removeBtn.innerHTML = ("<i class='fas fa-trash-alt'></i>");
+        removeBtn.classList.add('removeBtn');
+
+        read_form.appendChild(itemBox);
+
+        itemBox.appendChild(noteBody);
+        itemBox.appendChild(removeBtn);
+
+        removeBtn.addEventListener('click', () => this.remove(itemBox));
+    }
+
+    remove(item){
+        noteBody.removeChild(item);
+    }
+
+}
+
+
+note_button.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.which == 13){
+		check();
+	}
+})
+
+function check(){
+    if(noteBody.value != ""){
+        new note_display(noteBody.value);
+        noteBody.value = "";
+    }
+}
+
+
