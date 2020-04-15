1aac2ca3e61f38e12b6337157b52d7f811a2f8ab
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-15 12:12:43 +0200

    deleted search.js file. put everything in maps.js. added code to maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -113,10 +113,81 @@ function initMap() {
         stylers: [{ color: '#92998d' }]
       }
     ],
-
   });
 
+let search = document.getElementsByClassName('.search_wrapper');
+let input = document.getElementById('#search-input');
 
+let autocomplete = new google.maps.places.Autocomplete(input);
+
+// Bind the map's bounds (viewport) property to the autocomplete object,
+// so that the autocomplete requests use the current map bounds for the
+// bounds option in the request.
+autocomplete.bindTo('bounds', map);
+
+// Set the data fields to return when the user selects a place.
+autocomplete.setFields(
+    ['address_components', 'geometry', 'icon', 'name']);
+
+let infowindow = new google.maps.InfoWindow();
+let infowindowContent = document.getElementById('infowindow-content');
+infowindow.setContent(infowindowContent);
+let marker = new google.maps.Marker({
+  map: map,
+  anchorPoint: new google.maps.Point(0, -29)
+});
+
+autocomplete.addListener('place_changed', function() {
+  infowindow.close();
+  marker.setVisible(false);
+  let place = autocomplete.getPlace();
+  if (!place.geometry) {
+    // User entered the name of a Place that was not suggested and
+    // pressed the Enter key, or the Place Details request failed.
+    window.alert("No details available for input: '" + place.name + "'");
+    return;
+  }
+
+  // If the place has a geometry, then present it on a map.
+  if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+  } else {
+    map.setCenter(place.geometry.location);
+    map.setZoom(17);  // Why 17? Because it looks good.
+  }
+  marker.setPosition(place.geometry.location);
+  marker.setVisible(true);
+
+  let address = '';
+  if (place.address_components) {
+    address = [
+      (place.address_components[0] && place.address_components[0].short_name || ''),
+      (place.address_components[1] && place.address_components[1].short_name || ''),
+      (place.address_components[2] && place.address_components[2].short_name || '')
+    ].join(' ');
+  }
+
+  infowindowContent.children['place-icon'].src = place.icon;
+  infowindowContent.children['place-name'].textContent = place.name;
+  infowindowContent.children['place-address'].textContent = address;
+  infowindow.open(map, marker);
+});
+
+/*-----------------------------------------------------*/
+//a click listner for the radio buttons to change
+//the lockations whole sweden , north and  south
+function setupClickListener(id, types){
+  let radioButton = document.getElementById(id);
+  radioButton.addEventListener('click', function(){
+    autocomplete.setTypes(types);
+  });
+}
+setupClickListener('whole',[]);        //view-all
+setupClickListener('north',['north']); //view-north
+setupClickListener('south',['south']); //view-south
+/*-----------------------------------------------------*/
+
+/*-----------------------------------------------------*/
   let locations = [
     { lat: 66.929681, lng: 20.220471 }, //Nationalpark
     { lat: 67.283337, lng: 17.700021 }, //Nationalpark
@@ -137,4 +208,12 @@ function initMap() {
 
 }
 
+/*--------------a reset click function----------------*/
+function reset() {
+  $('#category')[0].selectedIndex = 0;
+  $("#search-input").val("");
+  $('#locationContainer').html("");
+  place = "";
+}
+/*-----------------------------------------------------*/
 
