1f3edaf7bf3e654a6319f9f89bcc30b030e99985
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-16 13:53:11 +0200

    added functions to contact.js to receive email.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,41 +1,48 @@
 /*I obtained information for this contact from here*/
 /*https://www.youtube.com/watch?v=WY4rvU4ImgE */
 
-function validation(){
-    
-   var name = document.getElementById("name").value;
-   var email = document.getElementById("email").value;
-   var message = document.getElementById("message").value;
-   var error_message = document.getElementById("error_message");
-   
+function validation(contactForm){
+  
+   let name = document.getElementById("name").value;
+   let email = document.getElementById("email").value;
+   let message = document.getElementById("message").value;
+   let error_message = document.getElementById("errorMessage");
+
    error_message.style.padding = "10px";
    
-   var text;
+   //Name needs to contain more than 1 letter
+   let text;
    if(name.length < 1){
      text = "Please enter a valid name";
      error_message.innerHTML = text;
      return false;
    }
-   
+
+   //Email needs to contains @ and more than 5 letters
    if(email.indexOf("@") == -1 || email.length < 5){
      text = "Please enter a valid email";
      error_message.innerHTML = text;
      return false;
    }
+   //Message needs to contain more than 1 letter
    if(message.length <= 1){
      text = "Please enter some text";
      error_message.innerHTML = text;
      return false;
-   }
-   alert("Thanks for your message!");
-   return true;
-}
-
-function contactFunction() {
-  var x = document.getElementsByClassName("contact_wrapper");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+   } 
+   
+   var template_params = {
+    "from_name": contactForm.name.value,
+    "from_email": contactForm.email.value,
+    "message_html": contactForm.message.value
   }
+  
+  var service_id = "default_service";
+  var template_id = "template_47gjBkEs";
+  emailjs.send(service_id, template_id, template_params);
+   
+  alert("Thank you " + name + " for your message! We will contact you at " + email);
+
+  return true;
+   
 }
