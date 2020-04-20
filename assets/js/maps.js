c1a4994f6ff129f1489b893d397ddf67f3467f8f
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-20 16:38:30 +0200

    added pictures, to maps.js and google.maps location code to maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -133,17 +133,21 @@ function initMap() {
   $(document).ready(function(){
     $("#changetype-north").click(function(){
       //on click display all elements in north
-      $("locations");
+      $("locationsNorth");
       //on click display content
       $("markers").on('click',function(){
-        $("content");
+        $("contentNorth");
       });
     });
   
     
     $("#changetype-south").click(function(){
       //on click display all elements in south
-      alert("button south test");
+      $("locationsSouth");
+      //on click display content
+      $("markers").on('click',function(){
+        $("contentSouth");
+      });
     });
 
     $("#reset").click(function(){
@@ -158,42 +162,67 @@ function initMap() {
       map: map,
       anchorPoint: new google.maps.Point(0, -29)
   });
+
 /*------------location and content for North------------*/
-  let locations = [
-    ["Nationalpark1", 66.929681, 20.220471 ], //Nationalpark
-    ["Nationalpark2", 67.283337, 17.700021 ], //Nationalpark
-    ["Nationalpark3", 63.971291, 18.055406 ], //Nationalpark
-    ["Nationalpark4", 63.119522, 18.493946 ]  //Nationalpark
+  let locationsNorth = [
+    ["Muddus National Park", 66.929681, 20.220471 ], //Nationalpark
+    ["Sarek National Park", 67.283337, 17.700021 ], //Nationalpark
+    ["Björnlandets National Park", 63.971291, 18.055406 ], //Nationalpark
+    ["Skuleskogen National Park", 63.119522, 18.493946 ],  //Nationalpark
+    ["Pieljekaise National Park", 66.350583, 16.742662],  //Nationalpark
+    ["Sånfjället National Park", 62.299990, 13.583215 ],  //Nationalpark
+    ["Vadvetjåkka National Park", 68.539931, 18.434607 ]  //Nationalpark
   ];
   
   let content = [
-    ["Nationalpark1", "<div>" +
-    "<h3>text</h3>" +
-    "<p>text.</p>" +
-    "<p>text</p>" +
-    "<p>text<a  href='#' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
-    "<p></p>" +
+    ["Muddus National Park", "<div>" +
+    "<h3>Muddus National Park</h3>" +
+    "<p>Muddus / Muttos National Park is the vast marshes and deep ancient forests.</p>" +
+    "<p>There are large waterfalls, deep ravines, rocky outcrops and forests.</p>" +
+    "<p>For more information <a  href='https://naturkartan.se/sv/nationalparker/muddus-nationalpark' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+    "<p><img src='./assets/images/muddus-nationalpark.jpg'></p>" +
     "</div>"],
-    ["Nationalpark2", "<div>" +
-    "<h3>text</h3>" +
-    "<p>text.</p>" +
-    "<p>text</p>" +
-    "<p>text<a  href='#' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
-    "<p></p>" +
+    ["Sarek National Park", "<div>" +
+    "<h3>Sarek National Park</h3>" +
+    "<p>In Sarek there are our most dramatic markers; old forests, valleys covered by birch forest,</p>" +
+    "<p>delta, mountain heaths and high mountain masses with glaciers and 2000-meter peaks.</p>" +
+    "<p>For more information <a href='https://naturkartan.se/sv/nationalparker/sarek' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+    "<p><img src='./assets/images/sarek-nationalpark.jpg' class='rounded mx-auto d-block'></p>" +
     "</div>"],
-    ["Nationalpark3", "<div>" +
-    "<h3>text</h3>" +
-    "<p>text.</p>" +
-    "<p>text</p>" +
-    "<p>text<a  href='#' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
-    "<p></p>" +
+    ["Björnlandets National Park", "<div>" +
+    "<h3>Björnlandets National Park</h3>" +
+    "<p>Experience the magnificent wilderness in one of the country's most valuable primeval forests.</p>" +
+    "<p>In the national park of bearland there are two overnight berths, several resting places and an extensive trail system.</p>" +
+    "<p>For more information <a  href='https://naturkartan.se/sv/nationalparker/bjornlandets-nationalpark-2' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+    "<p><img src='./assets/images/björnlandets-nationalpark.jpg' class='rounded mx-auto d-block'></p>" +
     "</div>"],
-    ["Nationalpark4", "<div>" +
-    "<h3>text</h3>" +
-    "<p>text.</p>" +
-    "<p>text</p>" +
-    "<p>text<a  href='#' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
-    "<p></p>" +
+    ["Skuleskogen National Park", "<div>" +
+    "<h3>Skuleskogen National Park</h3>" +
+    "<p>In the Skuleskogen you will find one of the few remaining large forests in the coastal country.</p>" +
+    "<p>Here you can hike for days, enjoy lush brook forests or go out on the barren hillsides and be enchanted by the powerful views.</p>" +
+    "<p>For more information <a href='https://naturkartan.se/sv/nationalparker/skuleskogen-nationalpark' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+    "<p><img src='./assets/images/Skuleskogen-nationalpark.jpg' class='rounded mx-auto d-block'></p>" +
+    "</div>"],
+    ["Pieljekaise National Park", "<div>" +
+    "<h3>Pieljekaise National Park</h3>" +
+    "<p>Pieljekaise National Park is located in the Arjeplogsfjällen, between Jäkkvik and Adolfström. The highest mountain, 1,138 meters above sea level, is Pieljekaise, which also gave its national park its name.</p>" +
+    "<p>In Sami, the mountain is called Bieljijgájse, which means the ear tips.</p>" +
+    "<p>For more information <a href='https://naturkartan.se/sv/nationalparker/pieljekaise-nationalpark' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+    "<p><img src='./assets/images/pieljekaise-nationalpark.jpg' class='rounded mx-auto d-block'></p>" +
+    "</div>"],
+    ["Sånfjället National Park", "<div>" +
+    "<h3>Sånfjället National Park</h3>" +
+    "<p>Almost wherever you are in Härjedalen, Sonfjället's characteristic silhouette stands out on the horizon.</p>" +
+    "<p>This is the bear's home. The area is today regarded as one of the country's most densely bearde</p>" +
+    "<p>For more information <a href='https://naturkartan.se/sv/nationalparker/sanfjallet-nationalpark' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+    "<p><img src='./assets/images/sånfjället-nationalpark.jpg' class='rounded mx-auto d-block'></p>" +
+    "</div>"],
+    ["Vadvetjåkka National Park", "<div>" +
+    "<h3>Vadvetjåkka National Park</h3>" +
+    "<p>Vadvetjåkka is a small national park in a mountainous area northwest of Torneträsk at the border with Norway.</p>" +
+    "<p>The park is named after the mountain Vadvetjåkka, which dominates the area. Here you go if you want to experience the real solitude, a rich mountain flora and deep cave systems.</p>" +
+    "<p>For more information <a href='https://naturkartan.se/sv/nationalparker/vadvetjakka-nationalpark' target='_blank'><span class='sr-only'>Click Here</span>Click Here</a></p>" +
+    "<p><img src='./assets/images/vadvetjåkka-nationalpark.jpg' class='rounded mx-auto d-block'></p>" +
     "</div>"]
   ];
   /*-----------------------------------------------------*/
@@ -224,12 +253,14 @@ function initMap() {
 
 
   let markers = [];
-
-  for (var i = 0; i < locations.length; i++) {
+  var ginfowindow = new google.maps.InfoWindow({
+    maxHeight: 300
+});
+  for (var i = 0; i < locationsNorth.length; i++) {
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        position: new google.maps.LatLng(locationsNorth[i][1], locationsNorth[i][2]),
         map: map,
-        title: locations[i][0]
+        title: locationsNorth[i][0]
     });
 
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
