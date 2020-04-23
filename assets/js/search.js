cf9f88b48c652be957df35124d0616fd5aeb0143
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-04-23 19:02:52 +0200

    testing code and editing code in html.css and javascript

---
+++ assets/js/search.js
@@ -0,0 +1,40 @@
+
+// function for Whole Sweden radio button
+$("#changetype-all").click(function(){
+    $("#category").mouseup(function(){
+      let open = $(this).data("isopen");
+      if (open)/*display content from*/ {
+        alert("all")
+      }
+      $(this).data("isopen", !open);
+    });
+  });
+  
+  // function for north radio button
+  $("#changetype-north").click(function(){
+    alert("test north");
+    $("#category").mouseup(function(){
+      let open = $(this).data("isopen");
+      /*if (open){
+      }*/
+      $(this).data("isopen", !open);
+    });
+  });
+
+// function for south radio button
+$("#changetype-south").click(function(){
+    alert("test south")
+    $("#category").mouseup(function(){
+      let open = $(this).data("isopen");
+      $(this).data("isopen", !open);
+    });
+  });
+  
+
+  // Function to reset radio buttons,
+  // maps info and category selector.
+  $("#reset").click(function(){
+    alert("reset test");
+  });
+
+
